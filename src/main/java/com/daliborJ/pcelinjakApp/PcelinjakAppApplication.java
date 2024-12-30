86e17bfed082880b906fe0e8b3796db6db3a0e55
Dalibor Jankovic <dalibor.jan87@gmail.com>
2024-12-30 14:02:01 +0100

    Initial commit

---
+++ src/main/java/com/daliborJ/pcelinjakApp/PcelinjakAppApplication.java
@@ -0,0 +1,13 @@
+package com.daliborJ.pcelinjakApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcelinjakAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcelinjakAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daliborJ/pcelinjakApp/PcelinjakAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daliborJ.pcelinjakApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcelinjakAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
